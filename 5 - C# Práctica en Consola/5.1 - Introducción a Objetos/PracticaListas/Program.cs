92ccdf12ffc3b0f9d32c7b3d8c77772783250e20
leand <leandrojuan1998@gmail.com>
2023-09-30 04:02:31 -0300

    Practica de pila

---
+++ 5 - C# Práctica en Consola/5.1 - Introducción a Objetos/PracticaListas/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PracticaListas {
+    internal class Program {
+        static void Main(string[] args) {
+
+            List<string> listaLineas = new();
+            Stack<string> pilaLineas = new();
+            string linea;
+
+            Console.WriteLine("Ingrese 10 palabras");
+            for (int i = 0; i < 10; i++) {
+                do {
+                    Console.Write("Palabra " + (i + 1) + ": ");
+                    linea = Console.ReadLine();
+                } while (string.IsNullOrEmpty(linea));
+                listaLineas.Add(linea);
+                pilaLineas.Push(linea);
+            }
+
+            Console.WriteLine("Orden inverso - Lista");
+            while (listaLineas.Any()) {
+                Console.WriteLine(listaLineas.Last());
+                listaLineas.RemoveAt(listaLineas.Count - 1);
+            }
+
+            Console.WriteLine("Orden inverso - Pila");
+            while(pilaLineas.Any()) {
+                Console.WriteLine(pilaLineas.Pop());
+            }
+        }
+    }
+}
