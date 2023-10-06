3b6e32474e679a42c4fd03909367ff312c245404
lsandoval <lsandoval@VM-CONT04>
2023-10-06 12:27:16 -0300

    Agrego proyecto

---
+++ 8 - MVC/8.2 - Primera aproximación con Visual Studio/MVCApplication/Controllers/PopupController.cs
@@ -0,0 +1,55 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers {
+
+    namespace MVCApplication.Controllers {
+        public class PopupController : Controller {
+            // GET: Popup
+            public ActionResult Index ()
+            {
+                return View();
+            }
+
+            public string CargarPopup ()
+            {
+                DatosPopup datos = new()
+                {
+                    InSitu = true,
+                    Tercerizado = false,
+                    TiposTrabajo = new List<TipoTrabajo> {
+                    new TipoTrabajo() { Id = 1, Descripcion = "Tipo1" },
+                    new TipoTrabajo() { Id = 2, Descripcion = "Tipo2" },
+                    new TipoTrabajo() { Id = 3, Descripcion = "Tipo3" }
+                }
+                };
+                StringWriter sw = new();
+                ViewEngineResult ver = ViewEngines.Engines.FindPartialView(this.ControllerContext, "DatosPopup");
+                this.ViewData.Model = datos;
+                ViewContext vc = new(this.ControllerContext, ver.View, this.ViewData, this.TempData, sw);
+                ver.View.Render(vc, sw);
+                return sw.GetStringBuilder().ToString();
+            }
+
+            public string GuardarDatosPopup (DatosPopup datosPopup)
+            {
+                string respuesta = " ";
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        respuesta = "TODO OK!!!"; // TODO: Realizar las operaciones necesarias de la ViewModel
+                    }
+                }
+                catch (Exception ex)
+                {
+                    respuesta = ex.Message;
+                }
+                return respuesta;
+            }
+        }
+    }
+}
---
+++ 8 - MVC/8.2 - Primera aproximación con Visual Studio/MVCApplication/Models/ProvinciaModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models {
+    public class ProvinciaModel {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Descripcion {  get; set; }
+    }
+}
