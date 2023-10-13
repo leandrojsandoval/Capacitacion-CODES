a1bf5096ed82d738c24fa733bcbf36a994103b2d
leand <leandrojuan1998@gmail.com>
2023-10-13 20:35:56 -0300

    Practica Introduccion Vue

---
+++ 9 - Vue/Components/leandro/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="title">Leandro</h1>
+  <h2 class="subtitle">Single File Component</h2>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style></style>
---
+++ 9 - Vue/Components/leandro/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="content">
+    <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
